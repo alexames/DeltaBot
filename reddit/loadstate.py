f29819720f2059c4ee92d5cbdfd3f26401ff8068
William Yaworsky <wj.px01@gmail.com>
2013-07-03 21:03:31 -0400

    load/savestate.sh -> .py

---
+++ reddit/loadstate.py
@@ -0,0 +1,26 @@
+import os
+import paramiko
+import sys
+
+"""
+   This script is used to load database states of the local reddit instance.
+"""
+
+assert len(sys.argv) >= 2, 'Please pass the name of the state to load.'
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('127.0.0.1', port=2222, username='vagrant', password='vagrant')
+
+state_file_name = '/vagrant/states/%s.sql' % sys.argv[1]
+
+cmd             = 'sudo -u postgres psql -d reddit -f %s' % state_file_name
+
+print cmd
+
+_, _, c = client.exec_command(cmd)
+
+line = c.readline()
+while line:
+    sys.stdout.write(line)
+    line = c.readline()
