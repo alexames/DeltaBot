01dcd3ccf621dcc763d35cae39a491a33da70e92
David LeGare <excaliburhissheath@gmail.com>
2013-10-05 22:35:25 -0500

    Fixes typo in update_wiki_tracker

--- deltabot/deltabot.py
+++ deltabot/deltabot.py
@@ -432,7 +432,7 @@ class DeltaBot(object):
             # create link and format as markdown list item
             # "?context=2" means link shows comment earning the delta and the comment awarding it
             # "(1)" is the number of deltas earned from that comment (1 because this is the first delta the user has earned)
-            add_link = "\n\n* [%s](%s) (1)\n* [Awarded by %s](%s)" % (submission_title, submission_url, awarder_name, comment_url + "?context=2")
+            add_link = "\n\n[%s](%s) (1)\n* [Awarded by %s](%s)" % (submission_title, submission_url, awarder_name, comment_url + "?context=2")
             
             # combine header and link
             full_update = initial_text + add_link
