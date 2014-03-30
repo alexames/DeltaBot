f90cf0d1b3c3c750f9dc7222ff9c660274118fa3
Dubslow <bunslow@gmail.com>
2014-03-30 12:49:38 -0500

    An UNTESTED (2nd) incremental initial commit.
    I forgot I had to add changes before committing them... sigh.
    
    This adds Python 3 support, as well as completely overhauling the
    "check ancestor for points awarded code".
    
    Instead of what(ever) it did previously, now this does the following:
    For any potential point awarded,
      1) find the root of that whole comment tree
      2) search all comments in that tree for a DeltaBot confirmation message
            to the candidate awardee
    If such a previous confirmation message is found, then disallow the re-award,
    otherwise allow it. This is much simpler and much more thorough (I think).

--- deltabot/__main__.py
+++ deltabot/__main__.py
@@ -1,15 +1,26 @@
 import logging
 import deltabot
-import config
+#import config
+import json
 import os
 
 def sandbox():
     #FOR TESTING. Put whatever here.
     bot = deltabot.DeltaBot()
 
+# A whole new class just for some type checking was completely ridiculous
+def get_config(configFile):
+    if isinstance(configFile, dict):
+        return configFile
+    elif os.path.isfile(configFile):
+        with open(configFile) as cF:
+            return json.load(cF)
+    else:
+        return json.loads(configFile)
 
 def main():
-    c   = config.Config(os.getcwd() + '/config/config.json')
+    #c   = config.Config(os.getcwd() + '/config/config.json')
+    c = get_config(os.getcwd() + '/config/config.json')
     bot = deltabot.DeltaBot(c)
     bot.go()
 
