36664b8311827624e477e153e8d2d43e72563b09
Liam Costello <lrjcostello@gmail.com>
2014-04-01 00:23:01 +0200

    Formatted module. Changed 'configFile' param to 'config_file'.

--- deltabot/config.py
+++ deltabot/config.py
@@ -1,16 +1,18 @@
 import json
 import os
 
-# This object holds the configuration options for the bot
-class Config(object):
 
-    def __init__(self, configFile):
-        if isinstance(configFile, dict):
-            self.attrs = configFile
-        elif os.path.isfile(configFile):
-            self.attrs = json.load(open(configFile))
+class Config(object):
+    """
+    Holds the configuration options for the bot
+    """
+    def __init__(self, config_file):
+        if isinstance(config_file, dict):
+            self.attrs = config_file
+        elif os.path.isfile(config_file):
+            self.attrs = json.load(open(config_file))
         else:
-            self.attrs = json.loads(configFile)
+            self.attrs = json.loads(config_file)
 
     def __getattr__(self, name):
         return self.attrs.get(name)
